cd49dfea54f950b6d52e69ba0e71d0fb5558c7cd
Rajat Waghare <rajat@mediastic.in>
2026-03-21 03:55:46 +0530

    font case update

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col justify-between py-32 px-16 sm:items-start">
         <Image alt="WRIKSH Font" src="/logo-black.svg" width={100} height={20} priority />
         <div className="flex flex-col items-center gap-6 sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 tracking-tight text-black">Beauty Awaknes The Soul To Act.</h1>
+          <h1 className="max-w-xs text-3xl leading-10 tracking-tight text-black">Beauty awaknes the soul to act.</h1>
           <p className="max-w-md text-lg leading-8">Search for what is good and strong and beautiful in your society and elaborate from there. Push outward. Always create from what you already have. Then you will know what to do.</p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
